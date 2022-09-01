6fcf1c92eddc900a079862da293cd55e316b4614
RAYN\goni9 <goni9071@withvus.com>
2022-09-01 10:07:18 +0900

    PrettyLog 제거 및 Pageable을 SearchParameter로 귀속

--- src/main/resources/template/BaseDao.java
+++ src/main/resources/template/BaseDao.java
@@ -1,58 +1,54 @@
-package %packageName%.dao.base;
+package%packageName%.dao.base;
 
 import java.io.Serializable;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import %packageName%.util.PrettyLog;
+import%packageName%.util.PrettyLog;
 
 public interface BaseDao<T, ID extends Serializable> {
-  default List<T> selectList(PrettyLog prettyLog) {
-    return selectList(null, null, prettyLog);
+
+  default List<T> selectList() {
+    return selectList(null);
   }
 
-  default List<T> selectList(@Param("pageable") CustomPageable pageable, PrettyLog prettyLog) {
-    return selectList(null, pageable, prettyLog);
+  default List<T> selectList(SearchParameter searchParameter) {
+    return selectList(searchParameter);
   }
 
-  default List<T> selectList(@Param("scParam") SearchParameter searchParameter, PrettyLog prettyLog) {
-    return selectList(searchParameter, null, prettyLog);
+  public List<T> selectList(SearchParameter searchParameter);
+
+  public T selectOne(@Param("id") final ID id);
+
+  public T selectOne(SearchParameter searchParameter);
+
+  public long selectListCount(SearchParameter searchParameter);
+
+  default long selectListCount() {
+    return selectListCount(null);
   }
 
-  public List<T> selectList(@Param("scParam") SearchParameter searchParameter,
-      @Param("pageable") CustomPageable pageable, PrettyLog prettyLog);
+  public int insert(@Param("entity") T t);
 
-  public T selectOne(@Param("id") final ID id, PrettyLog prettyLog);
-
-  public T selectOne(@Param("scParam") SearchParameter searchParameter, PrettyLog prettyLog);
-
-  public long selectListCount(@Param("scParam") SearchParameter searchParameter, PrettyLog prettyLog);
-
-  default long selectListCount(PrettyLog prettyLog) {
-    return selectListCount(null, prettyLog);
-  }
-
-  public int insert(@Param("entity") T t, PrettyLog prettyLog);
-
-  public int update(@Param("entity") T t, PrettyLog prettyLog);
+  public int update(@Param("entity") T t);
 
   /**
    * 물리적 삭제처리
-   * 
+   *
    * @param id
    * @param prettyLog
    * @return
    */
-  public int delete(@Param("id") ID id, PrettyLog prettyLog);
+  public int delete(@Param("id") ID id);
 
   /**
    * 논리적 삭제 처리
-   * 
+   *
    * @param id
    * @param delId
    * @param prettyLog
    * @return
    */
-  public int delete(@Param("id") ID id, @Param("delId") String delId, PrettyLog prettyLog);
+  public int delete(@Param("id") ID id, @Param("delId") String delId);
 }
