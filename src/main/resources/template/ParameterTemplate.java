786d34e6e5591b2d1fcc83631bca7c4582e3a210
goni9071 <goni9071@withvus.ai>
2024-03-26 11:35:44 +0900

    파라미터 템플릿에 롬복 빌더 추가.

--- src/main/resources/template/ParameterTemplate.java
+++ src/main/resources/template/ParameterTemplate.java
@@ -1,11 +1,20 @@
 package %packageName%.parameter;
 
+import java.util.Date;
+
+import ai.withvus.pnd.api.parameter.ApiUserParam;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 [IMPORT]
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class %entityName%Param extends SearchParameter {
 %classContents%
 }
