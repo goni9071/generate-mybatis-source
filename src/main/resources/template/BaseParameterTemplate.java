8f8b0a113f5bce050dc2be084a5da84ff4c8d0c9
goni9071 <goni9071@withvus.ai>
2024-07-09 15:19:09 +0900

    BaseParameterTemplate.java SearchParameter import 추가.

--- src/main/resources/template/BaseParameterTemplate.java
+++ src/main/resources/template/BaseParameterTemplate.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import %packageName%.parameter.SearchParameter;
 [IMPORT]
 
 
